3499425f9811371b9f01d305a674672edc155f69
Hafsha7022 <66520172+HafshaMalwat@users.noreply.github.com>
2023-07-31 12:11:02 +0530

    Changes

---
+++ husain-portfolio/src/components/Home/index.js
@@ -0,0 +1,72 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Loader from 'react-loaders'
+import LogoTitle from '../../assets/images/letter-h.png'
+import LogoM from '../../assets/images/letter-m.png'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import Layout from '../Layout'
+
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  const nameArray = ['u', 's', 'a', 'i', 'n', ' ']
+  const nameArray2 = [ 'a', 'l', 'w', 'a','t']
+  const jobArray = [
+    'A',
+    '',
+    'S',
+    't',
+    'u',
+    'd',
+    'e',
+    'n',
+    't',
+    '.',
+  ]
+
+
+  
+
+
+    return (
+        <>
+        <Layout />
+          <div className="container home-page">
+            
+            <div className="text-zone">
+              <h1>
+              <span className={letterClass}>H</span>
+              <span className={`${letterClass} _12`}>i,</span>
+            <br />
+              <span className={`${letterClass} _13`}>I</span>
+              <span className={`${letterClass} _14`}>'m</span>
+                <img src={LogoTitle} alt="name"/>
+
+                <AnimatedLetters letterClass={letterClass}
+                  strArray={nameArray}
+                  idx={15} />
+                <img src={LogoM} alt="Surname"/>
+                <AnimatedLetters letterClass={letterClass}
+                  strArray={nameArray2}
+                  idx={16} />
+                <br />
+                <AnimatedLetters letterClass={letterClass}
+                  strArray={jobArray}
+                  idx={13} />
+              </h1>
+              <h2>Front End Developer / JavaScript Expert / Youtuber</h2>
+              <Link to="/contact" className="flat-button">
+                CONTACT ME
+              </Link>
+            </div>
+            
+          </div>
+    
+          
+        </>
+      )
+}
+
+export default Home
---
+++ husain-portfolio/src/components/SideBar/index.js
@@ -0,0 +1,54 @@
+import {Link, NavLink} from 'react-router-dom'
+import './index.scss'
+import LogoS from '../../assets/images/H.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {
+    faLinkedin,
+    faGithub,
+    faYoutube,
+    faSkype,
+  } from '@fortawesome/free-brands-svg-icons'
+  import {
+    faHome,
+    faUser,
+    faEnvelope,
+    faSuitcase,
+    faBars,
+    faClose,
+  } from '@fortawesome/free-solid-svg-icons'
+
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt='logo'/>
+            <img className="sub-logo" src={LogoSubtitle} alt='slobodan'/>
+        </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome}   color="4d4d4e" />
+            </NavLink>
+            
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="4d4d4e" />
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/husain-malwat-371207226/'>
+                <FontAwesomeIcon icon={faLinkedin} className="anchor-icon" color='#4d4d4e'/>
+                </a>
+            </li>
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/husain-malwat-371207226/'>
+                <FontAwesomeIcon icon={faGithub} className="anchor-icon" color='#4d4d4e'/>
+                </a>
+            </li>
+        </ul>
+    </div>
+)
+
+export default Sidebar
